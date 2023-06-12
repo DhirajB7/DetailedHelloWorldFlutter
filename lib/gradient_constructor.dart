195251542cd82e30aeb1fd0cd1ed8acc531da498
DhirajB7 <dhiraj.basavaraju@gmail.com>
2023-06-12 18:05:12 +0530

    first commit

---
+++ lib/gradient_constructor.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.redAccent,
+          Colors.greenAccent,
+          Colors.purpleAccent,
+          Colors.yellowAccent
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/gradient_constructor.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+  home: Scaffold(
+    body: GradientContainer(),
+  ),
+));
+}
